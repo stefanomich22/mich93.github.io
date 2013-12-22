9a4d7c1d302598d43e13bb04149f9613fccf9e1a
Stefano <stefano@ideapad>
2013-12-22 19:00:27 +0100

    comment box

--- comments/include/sql/update_1.4.1-2.0.0.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `{prefix}comment` ADD `comment_rating` INT( 12 ) NOT NULL DEFAULT '0';
-ALTER TABLE `{prefix}identifier` ADD `identifier_rating_value` INT( 12 ) NOT NULL DEFAULT '0';
-ALTER TABLE `{prefix}identifier` ADD `identifier_rating_number` INT( 12 ) NOT NULL DEFAULT '0';
